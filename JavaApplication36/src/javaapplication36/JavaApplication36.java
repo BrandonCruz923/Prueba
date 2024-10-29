6e07d32c4c9c16aec771dc6ce65d5d394c9f67a1
capriimanuel55 <capriimanuel55@gmail.com>
2024-10-29 12:29:12 -0600

    primera rama
    Primer commit

---
+++ JavaApplication36/src/javaapplication36/JavaApplication36.java
@@ -0,0 +1,20 @@
+
+package javaapplication36;
+
+/**
+ *
+ * @author capri
+ */
+public class JavaApplication36 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String Hola=("Esto solo es una pruebAa");
+        System.err.println("Mensaje"+Hola);
+    }
+    
+}
