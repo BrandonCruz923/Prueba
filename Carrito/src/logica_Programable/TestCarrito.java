509dc23db4174423631516ccf93261fc74defbd7
capriimanuel55 <capriimanuel55@gmail.com>
2024-11-11 21:45:32 -0600

    ultimo commit
    terminado

--- Carrito/src/logica_Programable/TestCarrito.java
+++ Carrito/src/logica_Programable/TestCarrito.java
@@ -4,6 +4,8 @@
  */
 package logica_Programable;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author capri
@@ -30,7 +32,7 @@ public class TestCarrito extends javax.swing.JFrame {
         btnRecomendar = new javax.swing.JButton();
         btnAltaProducto = new javax.swing.JButton();
         btnConsulta = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        btnEliminar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,8 +53,18 @@ public class TestCarrito extends javax.swing.JFrame {
         });
 
         btnConsulta.setText("Consulta de Productos");
+        btnConsulta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConsultaActionPerformed(evt);
+            }
+        });
 
-        jButton3.setText("Eliminar Producto");
+        btnEliminar.setText("Eliminar Producto");
+        btnEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEliminarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -69,7 +81,7 @@ public class TestCarrito extends javax.swing.JFrame {
                             .addComponent(btnConsulta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(btnRecomendar)
                             .addComponent(btnAltaProducto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(btnEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(96, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -82,7 +94,7 @@ public class TestCarrito extends javax.swing.JFrame {
                 .addGap(32, 32, 32)
                 .addComponent(btnConsulta)
                 .addGap(28, 28, 28)
-                .addComponent(jButton3)
+                .addComponent(btnEliminar)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
                 .addComponent(btnRecomendar)
                 .addGap(57, 57, 57))
@@ -92,13 +104,22 @@ public class TestCarrito extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnRecomendarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRecomendarActionPerformed
-        //carritoCompras.recomendaciones;
+        carritoCompras.recomendarProducto();
     }//GEN-LAST:event_btnRecomendarActionPerformed
     Carrito carritoCompras = new Carrito();//creamos objeto carrito comparas
     String producto; //variabale string producto
     private void btnAltaProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAltaProductoActionPerformed
-        
+        producto=JOptionPane.showInputDialog("Ingrese el producto al STACK :");//generamos altas en la pila stack
+        carritoCompras.agregarProducto(producto);//parte de la alta        
     }//GEN-LAST:event_btnAltaProductoActionPerformed
+
+    private void btnConsultaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConsultaActionPerformed
+       carritoCompras.mostrarCarrito();// metodo que lo muestra, solo lo mandamos a llamar
+    }//GEN-LAST:event_btnConsultaActionPerformed
+
+    private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
+       carritoCompras.eliminarUltimoProducto(); //metodo que elimina los productos
+    }//GEN-LAST:event_btnEliminarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -138,8 +159,8 @@ public class TestCarrito extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAltaProducto;
     private javax.swing.JButton btnConsulta;
+    private javax.swing.JButton btnEliminar;
     private javax.swing.JButton btnRecomendar;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
